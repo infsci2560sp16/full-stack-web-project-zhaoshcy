2c91092929ae7544e70701b824256c82dcb2bfe9
zhaoshcy <zhaoshcy@gmail.com>
2016-03-31 19:35:08 -0400

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,9 +7,10 @@ public class Main {
 
   public static void main(String[] args) {
 
-//    port(Integer.valueOf(System.getenv("PORT")));
+    port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
-	  get("/hello", (req, res) -> "Hello World");
+	
+	//get("/hello", (req, res) -> "Hello World");
     //Object r = new JavaGettingStarted();
     //Object r = new Week6Routes();
     //Object r = new Week7Routes();
